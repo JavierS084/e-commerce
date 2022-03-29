c8b20332f1587704918c7ac27feb51102a585685
JavierS084 <javier.sanchezb084@gmail.com>
2022-03-29 12:11:50 -0400

    V0.2

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,88 +1,53 @@
 
 function App() {
   return (
-    <div class="grid-container">
-        <header class="row">
+    <div className="grid-container">
+        <header className="row">
             <div>
-                <a class="brand" href="index.html">Zhort</a>
+                <a className="brand" href="index.html">Zhort</a>
             </div>
             <div>
-                <a href="cart.html"><i class="fa fa-cart-plus"></i></a>
+                <a href="cart.html"><i className="fa fa-cart-plus"></i></a>
                 <a href="signin.html">Iniciar Sesi&oacute;n</a>
             </div>
         </header>
         <main> 
              
-            <div class="row center">
-                <div class="card">
-                    <a href="product.html"><img class="medium" src="./images/product-1.jpeg" alt="Product"/></a>
-                    <div class="card-body">
+            <div className="row center">
+                <div className="card">
+                    <a href="product.html"><img className="medium" src="./images/product-1.jpeg" alt="Product"/></a>
+                    <div className="card-body">
                         <a href="product.html">
                             <h2>Chamarra Adidas </h2>
                         </a>
-                        <div class="rating">
-                            <i class="las la-battery-three-quarters"></i>
-                            <span><i class="fa fa-star"></i> </span>
-                            <span><i class="fa fa-star"></i> </span> 
-                            <span><i class="fa fa-star"></i> </span>
-                            <span><i class="fa fa-star"></i> </span> 
-                            <span><i class="fa fa-star"></i> </span>
+                        <div className="rating">
+                            <i className="las la-battery-three-quarters"></i>
+                            <span><i className="fa fa-star"></i> </span>
+                            <span><i className="fa fa-star"></i> </span> 
+                            <span><i className="fa fa-star"></i> </span>
+                            <span><i className="fa fa-star"></i> </span> 
+                            <span><i className="fa fa-star"></i> </span>
                             
                            
                         </div>
-                        <div class="price">
+                        <div className="price">
                             $90
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <a href="product.html"><img class="medium" src="./images/product-1.jpeg" alt="Product"/></a>
-                    <div class="card-body">
-                        <a href="product.html">
-                            <h2>Chamarra Adidas </h2>
-                        </a>
-                        <div class="rating">
-                            <span><i class="fa fa-star"></i> </span>
-                            <span><i class="fa fa-star"></i> </span> 
-                            <span><i class="fa fa-star"></i> </span>
-                            <span><i class="fa fa-star"></i> </span> 
-                            <span><i class="fa fa-star"></i> </span>
-                           
-                        </div>
-                        <div class="price">
-                            $90
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <a href="product.html"><img class="medium" src="./images/product-1.jpeg" alt="Product"/></a>
-                    <div class="card-body">
-                        <a href="product.html">
-                            <h2>Chamarra Adidas </h2>
-                        </a>
-                        <div class="rating">
-                            <span><i class="fa fa-star"></i> </span>
-                            <span><i class="fa fa-star"></i> </span> 
-                            <span><i class="fa fa-star"></i> </span>
-                            <span><i class="fa fa-star"></i> </span> 
-                            <span><i class="fa fa-star-o"></i></span>
-                           
-                        </div>
-                        <div class="price">
-                            $90
-                        </div>
-                    </div>
-                </div>
-
+               
                 
             </div>     
    
         </main>
-        <footer class="row center">
-            All rights reserved
+        <footer className="row center">
+            
+        <p>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Javier S&aacute;nchez. Todos los derechos reservados.</p>
+  
+
         </footer>
     </div>
+    
   );
 }
 
