34ca8424a4687fa5efb925a35a392ea4546b215a
JavierS084 <javier.sanchezb084@gmail.com>
2022-04-03 12:40:08 -0400

    V0.5

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -8,7 +8,7 @@ function App() {
             </div>
             <div>
                 <a href="cart.html"><i className="fa fa-cart-plus"></i></a>
-                <a href="signin.html">Iniciar Sesi&oacute;n</a>
+                <a class="btn btn-outline" href="signin.html">Iniciar Sesi&oacute;n</a>
             </div>
         </header>
         <main> 
