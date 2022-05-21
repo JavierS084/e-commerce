0e7d2869032eeab1f5dbc3f6874f09bb8ce60215
JavierS084 <javier.sanchezb084@gmail.com>
2022-05-21 11:37:07 -0400

    V 1.5

--- fronted/src/screens/CartScreen.js
+++ fronted/src/screens/CartScreen.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
 
 export default function cartScreen(props) {
-const productId = props.match.params.id;
-const qty = props.location.serach? Number(props.location.serach.split('=')[1]) : 1;
-  return (
-    <div>
-        <h1>Carrito</h1>
-        <p>
-          ADD to CART : ProductID: {productId} Cantidad: {qty}
-        </p>
-    </div>
-  )
+  const productId = props.match.params.id;
+  const dispatch = useDispatch()
+  const qty = props.location.search
+    ? Number(props.location.search.split('=')[1])
+    :1;
+
+  useEffect(() => {
+    if (productId){
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+    return (
+      <div>
+          <h1>Carrito</h1>
+          <p>
+            ADD to CART : ProductID: {productId} Cantidad: {qty}
+          </p>
+      </div>
+    );
 }
